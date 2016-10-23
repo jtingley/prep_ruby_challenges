0241d578a562f3cbee9224c8ae3d18e3cda88612
Jake Tingley <jaket32@gmail.com>
2016-10-23 12:30:58 -0400

    finished countinggame.rb

--- countinggame.rb
+++ countinggame.rb
@@ -1,14 +1,25 @@
-10 friends are sitting in a circle around a table and decide to play a new game. In it, they count up through the numbers from 1 to 100. The first person says "1", the second says "2" and so on... but with a few catches:
+def counting_game(total_players, high_count)
+  clockwise = true
+  person = 0
 
-Whenever the number is divisible by 7, they switch directions. So person 6 will say "6", person 7 will say "7", then person 6 again will say "8".
-Whenever the number is divisible by 11, they skip the next person for the following number. For instance, if person 3 says "33", person 5 will say "34" instead (person 4 gets skipped).
-Your job is to code a program which outputs each number and which person said it. Use it to show that player 1 will say the number "100".
+  high_count.times do |count|
+    puts "Player " + (person+1).to_s + " says " + (count+1).to_s
+  
+    if (count + 1) % 7 == 0
+      clockwise = !clockwise
+    end
+  
+    if (count + 1) % 11 == 0  
+      jump = 2 
+    else jump = 1
+    end
+  
+    if clockwise
+      person = (person + jump) % total_players
+    else person = (person - jump) % total_players
+    end
+    
+  end
+end
 
-Tips:
-
-Remember to stick with brute force instead of trying to "figure out" the trick to the problem.
-Name your variables well!
-Ignore the skipping to start out with. Only add it when you're ready.
-Advanced Option:
-
-Make your method take two inputs -- the number of players and the number you're counting up to. Then see who says the last number each time!
+counting_game(10, 100)
